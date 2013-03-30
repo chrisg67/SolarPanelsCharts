51e48cc3791417f3d4ab082b328d7ec9892b7198
Chris Goldsmith <chrisg67@server.fake>
2013-03-30 10:30:28 +0000

    gernericised addToDatabase

--- pi/addToDatabase.py
+++ pi/addToDatabase.py
@@ -2,12 +2,13 @@ import sqlite3
 import sys
 import re
 
-def main():
+def update_db(filename):
   p = re.compile("^(\d+)/(\d+)/(\d+) (\d+):(\d+):(\d+)\s+total=([0-9.]+)\s+Kwh\s+current=(\d+)\s+Watts\s+togo=\d+\s+i=\d+\s+crc=\d+")
   conn = sqlite3.connect('..\\dB\\test.db')
   c = conn.cursor()
-  with open('output', 'r') as f:
+  with open(filename, 'r') as f:
     line = f.readline()
+    old_date=''
     while line:
       m = p.match(line)
       if m is not None:
@@ -19,18 +20,30 @@ def main():
         second = m.group(6)
         total_power_kWh = m.group(7)
         power_kW = float(m.group(8))/1000
-        time = year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second
+        date = year+'-'+month+'-'+day+
+        time = date+' '+hour+':'+minute+':'+second
         c.execute('SELECT * from five_minute_data WHERE time="'+time+'";')
         res = c.fetchone()
         if res is None:
+          # New piece of data
           cmd = 'INSERT INTO five_minute_data VALUES (' + \
                  '"'+time+'",' + total_power_kWh + ', ' + str(power_kW) + ', ' + \
                  str(power_kW/12.0) + ');'
-          print cmd
           c.execute(cmd)
+          # If there is an entry for date already, then update the "complete" field for that day to
+          # incomplete
+          if date != old_date:
+            c.execute('SELECT complete FROM day_data WHERE date="'+date+'";')
+            res = c.fetchone()
+            if res is not None:
+              c.execute('UPDATE day_data SET complete=0 WHERE date="'+date+'";')
+        old_date = date
       line = f.readline()
   conn.commit()
   conn.close()
+
+def main():
+  update_db('output')
 
 if __name__ == "__main__":
   main()
