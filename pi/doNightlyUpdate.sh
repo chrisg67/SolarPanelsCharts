1a70a626b3d693135243df8e80383ec3d8395691
Chris Goldsmith <chrisg67@server.fake>
2013-04-01 08:35:42 +0100

    Added new pi scripts to update dB

--- pi/doNightlyUpdate.sh
+++ pi/doNightlyUpdate.sh
@@ -3,6 +3,6 @@ set -e
 if [ `date +%Y` -ge 2013 ] ; then 
   ./grabAll.sh
   ./convertDayData.sh
-  ./doCharts.sh
+  ./updateDb.sh
   ./uploadNightly.sh
 fi
--- pi/uploadNightly.sh
+++ pi/uploadNightly.sh
@@ -2,4 +2,5 @@
 set -e
 tar czvf currentData.tgz currentData
 ./dropbox_uploader.sh upload currentData.tgz "Power/PV/currentData.tgz"
+./dropbox_uploader.sh upload panels.db "Power/PV/panels.db"
 
